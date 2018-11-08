d284d6bc7317c3b71c54f5cb9c36a1e2b9454031
Florian Rogeret <florian.rogeret@outlook.fr>
2018-11-08 16:42:08 +0100

    Avancement du projet jusqu'au TP7

--- unicorn/games_review/admin.py
+++ unicorn/games_review/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from games_review.models import *
 
-# Register your models here.
+admin.site.register(Type)
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'release_date', )
+    search_fields = ('name', )
+    list_filter = ('types', )
+
+admin.site.register(Game, GameAdmin)
